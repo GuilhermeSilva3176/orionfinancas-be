9b4bac886eb397827da03dd90042d8c2b3582fbd
Guilherme <144273334+GuilhermeSilva3176@users.noreply.github.com>
2026-04-28 21:25:20 -0300

    fix: global cors origin

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
